854b496777db993214d3194fccc9bcaf3c0ef39f
promise.li <promise.li@corp.to8to.com>
2020-03-15 12:08:06 +0800

    add gulp-if

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp')
+var gulpif = require('gulp-if')
 var sequence = require('run-sequence')
 var browserify = require('browserify')
 var watchify = require('watchify')
 var buffer = require('vinyl-source-buffer')
 var uglify = require('gulp-uglify')
+
+var isPrd = process.env.ENV === 'production'
 
 gulp.task('mainjs', (cb) => {
     var b = browserify({
@@ -15,10 +18,10 @@ gulp.task('mainjs', (cb) => {
 
     function bundle() {
         b
-        .bundle()
-        .pipe(buffer('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./build'))
+            .bundle()
+            .pipe(buffer('main.js'))
+            .pipe(gulpif(isPrd, uglify()))
+            .pipe(gulp.dest('./build'))
     }
 
     bundle()
