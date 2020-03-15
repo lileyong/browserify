026d710b32258e2b5b6df0de48e0b073fa430831
promise.li <promise.li@corp.to8to.com>
2020-03-15 11:42:31 +0800

    vinyl-source-buffer

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp')
 var sequence = require('run-sequence')
 var browserify = require('browserify')
 var watchify = require('watchify')
-var stream = require('vinyl-source-stream')
-var buffer = require('vinyl-buffer')
+var buffer = require('vinyl-source-buffer')
 var uglify = require('gulp-uglify')
 
 gulp.task('mainjs', (cb) => {
@@ -17,8 +16,7 @@ gulp.task('mainjs', (cb) => {
     function bundle() {
         b
         .bundle()
-        .pipe(stream('main.js'))
-        .pipe(buffer())
+        .pipe(buffer('main.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./build'))
     }
